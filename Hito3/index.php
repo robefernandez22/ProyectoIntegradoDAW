12543305589bf73c5c981e4274a3d7f1bb1103ba
Roberto Fernández Romero <33895301+robefernandez22@users.noreply.github.com>
2019-04-30 17:40:39 +0200

    Interfaz usuario
    Creación de la interfaz de usuario.

---
+++ Hito3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Hoteles ESE - Inicio</title>
+
+		<!-- Librería jQuery -->
+		<script src="./lib/jquery-3.4.0.min.js"></script>
+		<!-- Script principal -->
+		<script src="./scripts/js/main.js"></script>
+	</head>
+
+	<body>
+
+	</body>
+
+</html>
