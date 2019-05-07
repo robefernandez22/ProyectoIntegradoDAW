92ff5d7fb238f8e9d1e8ab2b36aa53c8aece58e3
robefernandez22 <robertofernandezromero61@gmail.com>
2019-05-07 13:46:01 +0200

    avance en interfaz

--- Hito3/scripts/js/main.js
+++ Hito3/scripts/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
-	
+	// Función necesaria para habilitar los elementos tooltip de Bootstrap
+	$("[data-toggle='tooltip']").tooltip({
+		container: 'body'
+	});
 
 });
